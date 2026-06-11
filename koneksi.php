d3032bdce7b16606520b911526b16620d38cf24d
ntayftr-debug <ntay.ftr@gmail.com>
2026-06-11 16:04:13 +0700

    perbaikan

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_ppba";
+// Detail koneksi database dari panel InfinityFree Anda
+$host = "sql311.infinityfree.com"; 
+$user = "if0_42155397";           
+$pass = "E4s5bJISKHc"; // Klik ikon mata di panel InfinityFree untuk melihat password Anda
+$db   = "if0_42155397_db_ppba";    // SESUAIKAN dengan nama database yang baru saja Anda buat di atas
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 if (!$koneksi) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi ke database hosting gagal: " . mysqli_connect_error());
 }
 ?>
